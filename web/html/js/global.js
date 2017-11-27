7c0bddc4dfc62489c57094d50740daec214829b2
nando <nandopolan@gmail.com>
2017-11-27 23:44:30 +0100

    resuelto problema mayus con simbolos especiales

--- web/html/js/global.js
+++ web/html/js/global.js
@@ -32,18 +32,22 @@ $("document").ready(function(){
     $("form").keypress(function (ev) {
         var keycode = (ev.keyCode ? ev.keyCode : ev.which);
         if (ev.shiftKey){                                       // Shift + ...
-            ev.preventDefault();
             if (keycode == '97' || keycode == '65') {           // A
-                alta();
+                ev.preventDefault();
+                alta();               
             }else if (keycode == '98' || keycode == '66') {     // B
+                ev.preventDefault();
                 baja();
             }else if (keycode == '109' || keycode == '77') {    // M
+                ev.preventDefault();
                 modificar();
             }else if (keycode == '108' || keycode == '76') {    // L
+                ev.preventDefault();
                 limpiar();
             }else if (keycode == '115' || keycode == '83') {    // S
+                ev.preventDefault();
                 salir();
-            }
+            }            
         }
     });
 
