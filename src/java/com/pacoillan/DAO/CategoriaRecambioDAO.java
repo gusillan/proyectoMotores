4502524663343efee0a8843108d36fe213bfb64e
Gustavo <Gustavo@PC_Torre>
2018-01-19 00:01:21 +0100

    Creacion de formulario de Categoría de Recambios

---
+++ src/java/com/pacoillan/DAO/CategoriaRecambioDAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pacoillan.DAO;
+
+import com.pacoillan.pojo.CategoriaRecambio;
+
+/**
+ *
+ * @author Gustavo
+ */
+
+public interface CategoriaRecambioDAO extends GenericDAO<CategoriaRecambio,Integer> {
+    
+}
---
+++ src/java/com/pacoillan/DAOHibernate/CategoriaRecambioDAOHibernate.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pacoillan.DAOHibernate;
+
+import com.pacoillan.DAO.CategoriaRecambioDAO;
+import com.pacoillan.pojo.CategoriaRecambio;
+
+/**
+ *
+ * @author Gustavo
+ */
+
+public class CategoriaRecambioDAOHibernate extends GenericDAOHibernate<CategoriaRecambio, Integer> implements CategoriaRecambioDAO {
+}
