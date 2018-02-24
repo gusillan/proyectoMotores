5fd95f2be8cd6ca7f27663a5f1da42aea76308b7
Gustavo <Gustavo@PC_Torre>
2018-02-24 16:20:41 +0100

    Creacion del POJO categoriaMO

---
+++ src/java/com/pacoillan/DAO/CategoriaMODAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pacoillan.DAO;
+
+import com.pacoillan.pojo.CategoriaMO;
+
+/**
+ *
+ * @author Gustavo
+ */
+
+public interface CategoriaMODAO extends GenericDAO<CategoriaMO,Integer> {
+    
+}
---
+++ src/java/com/pacoillan/DAOHibernate/CategoriaMODAOHibernate.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pacoillan.DAOHibernate;
+
+import com.pacoillan.DAO.CategoriaMODAO;
+import com.pacoillan.pojo.CategoriaMO;
+
+/**
+ *
+ * @author Gustavo
+ */
+
+public class CategoriaMODAOHibernate extends GenericDAOHibernate<CategoriaMO, Integer> implements CategoriaMODAO {
+}
